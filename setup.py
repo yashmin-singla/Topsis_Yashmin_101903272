f05a4ed8cc0af3304b0d5bc45da54634f6c95e73
Yashmin Singla <yashminsingla25@gmail.com>
2022-02-27 22:39:39 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+setup(
+  name = 'Topsis_Yashmin_101903272',        
+  packages = ['Topsis_Yashmin_101903272'],  
+  version = 'v1.2',      
+  license='MIT',       
+  description = 'Topsis Score and Rank generator',
+  author = 'Yashmin Singla',                
+  author_email = 'ysingla_be19@thapar.edu',      
+  url = 'https://github.com/yashmin-singla/Topsis_Yashmin_101903272',   
+  download_url = '',    
+  keywords = ['topsis','topsis score','rank','Thapar'],
+  install_requires=['numpy','pandas' ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',    
+    'Intended Audience :: Developers',   
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
